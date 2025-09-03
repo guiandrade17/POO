bf00eb6766085f55e4b07241c16e8836187dc67a
Guilherme Brito Andrade <guilhermebritoandrade17@gmail.com>
2025-09-03 09:07:47 -0300

    Create 05) naturais_vertical.py

---
+++ Aula 14-8/05) naturais_vertical.py
@@ -0,0 +1,5 @@
+print('- Números naturais na vertical:')                     # print('Cabeçalho')
+for i in range(0, 11, 1):    # for i in range (0, 10+1):     # for i in range(11):
+    print(i)                 # Mostra i na tela. O print quebra a linha por padrão
+    # Fim da estrutura de repetição for
+print('Encerrou a repetição.')
