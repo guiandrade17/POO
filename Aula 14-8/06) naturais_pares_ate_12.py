3155842e38c632f2f7592e477a5bb4cb9410d1d5
Guilherme Brito Andrade <guilhermebritoandrade17@gmail.com>
2025-09-03 09:08:56 -0300

    Create 06) naturais_pares_ate_12.py

---
+++ Aula 14-8/06) naturais_pares_ate_12.py
@@ -0,0 +1,5 @@
+print('- Números naturais pares:')      # Cabeçalho.
+for par in range(0, 13, 2):             # for par in range(0, 12+1, 2):
+    print(par)                          # Na vertical.
+    # Fim da estrutura de repetição for
+print('\nEncerrou a repetição.')
