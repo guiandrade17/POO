07863176229eb69d79fffd9c831064ec7839b150
Guilherme Brito Andrade <guilhermebritoandrade17@gmail.com>
2025-09-03 09:10:07 -0300

    Create 07) naturais_decrescente.py

---
+++ Aula 14-8/07) naturais_decrescente.py
@@ -0,0 +1,4 @@
+print('- Ordem decrescente:')        # Cabeçalho
+for decrescente in range(7, 0, -1):  # for decrescente in range(7, 0, -1):
+    print(decrescente)               # para cada range de 7 até 1, em saltos de -1
+print('\nEncerrou a repetição.')
